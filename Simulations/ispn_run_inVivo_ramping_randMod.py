2b86bb841f665733305ad280e26a67224f1a39b6
Robert @ ws <robert.lindroos@ki.se>
2020-06-09 23:32:05 +0200

    ERROR FIX: wrong morphology used in ispn branch. ---Fix: ispn morph added and selected in ispn_run_inVivo_ramping_randMod.py. should also be updated in other ispn sim files!

--- Simulations/ispn_run_inVivo_ramping_randMod.py
+++ Simulations/ispn_run_inVivo_ramping_randMod.py
@@ -44,7 +44,7 @@ def run_model(cell_index, ci):
 
     parameters      =   model_sets[cell_index]['variables'] 
     par             =   '../params_iMSN.json'
-    morphology      =   '../Morphologies/WT-dMSN_P270-20_1.02_SGA1-m24.swc'
+    morphology      =   '../Morphologies/WT-iMSN_P270-09_1.01_SGA2-m1.swc
 
     cell = build.MSN(  params=par,                  \
                        morphology=morphology,       \
